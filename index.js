e833b7c873ce2a2b7f7a767c5d246644a03e1661
Ganga Shanker Shukla <gangashankershukla02072004@gmail.com>
2024-11-11 09:17:24 +0530

    m

---
+++ index.js
@@ -0,0 +1,6 @@
+const para = document.createElement("p");
+para.id = "para-1";
+para.style.color = "red";
+para.style.backgroundColor = "yellow";
+para.innerHTML = "<h4> this is my paragraph </h4>"
+document.body.appendChild(para);
