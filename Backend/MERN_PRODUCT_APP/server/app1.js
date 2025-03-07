64f7a83eb5aeb7da177c1ebf37fbbd0c4531d4f1
Ganga Shanker Shukla <gangashankershukla02072004@gmail.com>
2025-03-07 11:41:10 +0530

    h

--- Backend/MERN_PRODUCT_APP/server/app1.js
+++ Backend/MERN_PRODUCT_APP/server/app1.js
@@ -1,60 +1,106 @@
-const express = require("express")
+const express = require("express");
+const fs = require("fs/promises");
+const app = express();
+const port = 3500;
+let users = [];
+const loadUsers = async () => {
+    try {
+        const userdata = await fs.readFile("./users.json", "utf-8");
+        console.log("After read file")
+        const usersjson = await userdata.json();
+        console.log("After JSON")
+        users = JSON.parse(usersjson);
+    }
+    catch (err) {
+        console.log("file load error")
+        users = [];
+    }
 
-const app = express()
-const PORT = 3000
-
-let users = []
-
+}
+const saveUser = async () => {
+    await fs.writeFile("users.json", JSON.stringify(users))
+}
+const m1 = (req, res, next) => {
+    const age = req.query.age;
+    if (!age) {
+        res.status(400).send('send age in query')
+    }
+    else {
+        if (age < 18) {
+            res.status(401).send('user not authorized')
+        }
+        else {
+            next();
+        }
+    }
+}
+// app.use(m1);
+app.use(express.json())
+loadUsers();
 app.get("/users", (req, res) => {
-    res.status(200).json(users)
+    res.status(200).json(users);
 })
 app.get("/user/:id", (req, res) => {
-    const uid = req.params.id
-    const idx = users.findIndex(user => user.id === uid)
-    if (idx === -1) {
-        res.status(404).json({ message: "User not found!" })
-    } else {
-        res.status(200).json({ message: "User found", data: users[idx] })
+    const uid = req.params.id;
+    const index = users.findIndex(user => user.id == uid);
+    if (index == -1) {
+        res.status(400).json({ message: "user not found" })
+    }
+    else {
+        res.status(200).json({ message: "user found", data: users[index] })
     }
 })
-app.post("/createuser", (req, res) => {
-    const { name: newName, email: newEmail } = req.body
-    if (!newName || !newEmail) {
-        res.status(400).json({ message: "Incomplete input data!" })
-    } else {
-        const newId = Date.now()
+app.post("/createuser", m1, (req, res) => {
+    const { name: newname, email: newemail } = req.body;
+    if (!newname || !newemail) {
+        res.status(400).json({ message: "Incomplete input" })
+    }
+    else {
+        const newId = Date.now();
         const newUser = {
-            id: newId,
-            name: newName,
-            email: newEmail
+            id: newId, name: newname, email: newemail
         }
-        users.push(newUser)
-        res.status(201).json({ message: "User created successfully!", data: newUser })
+        users.push(newUser);
+        saveUser();
+        res.status(201).json({ message: "user created successfully", data: newUser })
     }
-})
-app.put("/edituser/:id", (req, res) => {
-    const uid = req.params.id
-    const { name: newName, email: newEmail } = req.body
-    if (!newName || !newEmail) {
-        res.status(400).json({ message: "Invalid data" })
-    } else {
-        const idx = users.findIndex(user => user.id === uid)
-        if (idx === -1) {
-            res.status(404).json({ message: "User not found!" })
-        } else {
-            users[idx].name = newName
-            users[idx].email = newEmail
-            res.status(200).json({ message: "User updated successfully!", data: users[idx] })
-        }
-    }
-})
-app.delete("/user/:id", (req, res) => { })
 
-app.listen(PORT, (err) => {
-    try {
-        if (err) throw err
-        console.log(`Server running on port ${PORT}`)
-    } catch (err) {
-        console.log("Server Error: ", err.message)
+})
+app.put("/edituser/:id", m1, (req, res) => {
+    const uid = req.params.id;
+    const { name: newname, email: newemail } = req.body;
+    if (!newname || !newemail) {
+        res.status(400).json({ message: 'Invalid Input' })
     }
-})             
+    else {
+        const index = users.findIndex(user => user.id == uid);
+        if (index == -1) {
+            res.status(400).json({ message: 'User Id not Found' })
+        }
+        else {
+            users[index].name = newname;
+            users[index].email = newemail;
+            saveUser();
+            res.status(200).json({ message: "User Edited Successfully", data: users[index] })
+        }
+    }
+})
+app.delete("/user/:id", m1, (req, res) => {
+    const uid = req.params.id;
+    const index = users.findIndex(user => user.id == uid);
+    if (index == -1) {
+        res.status(400).json({ message: 'User Id not Found' })
+    }
+    else {
+        const deluser = users.splice(index, 1);
+        saveUser();
+        res.status(200).json({ message: "User Deleted Successfully", data: deluser })
+    }
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(Server is running on port ${ port })
+})
